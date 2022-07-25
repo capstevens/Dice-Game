4f26a1dbf8851cffe410034db526f8dc2d5f756c
Shivam <shivamsweetoo@gmail.com>
2022-07-25 22:23:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource = randomDiceImage;
+
+var randomImageSource2 = randomDiceImage2;
+
+document.querySelector(".img1").setAttribute("src",randomImageSource);
+
+document.querySelector(".img2").setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(randomNumber2>randomNumber1)
+{
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+else {
+  document.querySelector("h1").innerHTML = "It's A Tie";
+}
